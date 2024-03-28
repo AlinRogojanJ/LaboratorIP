280580c8d46470865c78ba3d159f0de35ec50421
Alin Rogojan Alin <alin_rogojan@yahoo.com>
2024-03-28 16:54:19 +0200

    App.js Rogojan

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Nistor</Text>
+      <Text>Rogojan</Text>
       <StatusBar style="auto" />
     </View>
   );
